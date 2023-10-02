a4676b7511d7d8469436add41a5f975e444d69ee
Laree Smith <smith.lar220@gmail.com>
2023-10-02 16:57:35 -0400

    Day1 Exercises

---
+++ Lab1.py
@@ -0,0 +1,41 @@
+# Exercise 1
+# prompt: str = 'Enter some text'
+# resp = input(prompt)
+# print(resp)
+
+# Exercise 2
+# prompt = input('Enter a number')
+# resp = int(prompt)
+# result = resp + 1
+# print(result)
+
+# Exercise 3
+# prompt = input('Enter a number')
+# resp = float(prompt)
+# result = resp + 0.5
+# print(result)
+
+# # Exercise 4
+# prompt1 = input('Enter a number')
+# resp1 = int(prompt1)
+# prompt2 = input('Enter a number')
+# resp2 = int(prompt2)
+# result = (resp1 + resp2)
+# print(result)
+
+# Exercise 5
+# prompt1 = input('Enter a number')
+# resp1 = int(prompt1)
+# prompt2 = input('Enter a number')
+# resp2 = int(prompt2)
+# result = (resp1 * resp2)
+# print(result)
+
+#Exercise 6
+# prompt1 = input('Enter a number')
+# resp1 = int(prompt1)
+# prompt2 = input('Enter a number')
+# resp2 = int(prompt2)
+# result = (resp1 / resp2)
+# print(result)
+
